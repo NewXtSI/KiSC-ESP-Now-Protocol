7fd869d3ec1def35ead5f9479909db5889e4477d
NewXtSI <leerzeichen32@googlemail.com>
2024-11-19 10:40:37 +0100

    Update MAC address definitions for controllers

--- include/members.h
+++ include/members.h
@@ -2,11 +2,12 @@
 #define INCLUDE_MEMBERS_INCLUDED
 
 // 98:F4:AB:DA:C6:05        Motorcontroller (ESP8266)
-// 98:F4:AB:DA:C6:05        Maincontroller (ESP32)
+// C8:C9:A3:FC:97:54        Maincontroller (ESP32)
 
 static uint8_t master[] = { 0x12, 0x34, 0x56, 0x78, 0x90, 0x12 };
+static uint8_t motor[] = { 0x98, 0xF4, 0xAB, 0xDA, 0xC6, 0x05 };
 
-#define MOTOR_CONTROLLER_MAC    {0x98, 0xF4, 0xAB, 0xDA, 0xC6, 0x05}
+#define MOTOR_CONTROLLER_MAC    motor
 #define MAIN_CONTROLLER_MAC     master
 
 
