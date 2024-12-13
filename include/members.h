fae004f7274498b7d2096b812528b4403d649cc5
Henning Schreiber <henning.schreiber@wago.com>
2024-12-13 16:52:34 +0100

    Add Sound Light Wrover MAC address and update controller definition

--- include/members.h
+++ include/members.h
@@ -7,6 +7,7 @@
 // 94:3C:C6:37:AF:44        Sound and Light Controller (ESP32)
 // 94:3C:C6:37:B3:20        Peripheral Controller (ESP32)
 // 94:3C:C6:38:9F:8C        BT Remote (ESP32)
+// B4:E6:2D:9C:DF:45        Sound Light Wrover
 static uint8_t master[] = { 0xC8, 0xC9, 0xA3, 0xFC, 0x97, 0x54 };
 static uint8_t motor[] = { 0x98, 0xF4, 0xAB, 0xDA, 0xC6, 0x05 };
 static uint8_t sound_light[] = { 0x94, 0x3C, 0xC6, 0x37, 0xAF, 0x44 };
@@ -14,10 +15,11 @@ static uint8_t peripheral[] = { 0x94, 0x3C, 0xC6, 0x37, 0xB3, 0x20 };
 static uint8_t remotecontrol[] = { 0x94, 0x3C, 0xC6, 0x37, 0xB3, 0x20 };
 static uint8_t motor2[] = { 0x48, 0x27, 0xE2, 0x51, 0xD6, 0xC0 };
 static uint8_t btremote[] = { 0x94, 0x3C, 0xC6, 0x38, 0x9F, 0x8C };
+static uint8_t sound_light_wrover[] = { 0xB4, 0xE6, 0x2D, 0x9C, 0xDF, 0x45 };
 
 #define MOTOR_CONTROLLER_MAC        motor2
 #define MAIN_CONTROLLER_MAC         master
-#define SOUND_LIGHT_CONTROLLER_MAC  sound_light
+#define SOUND_LIGHT_CONTROLLER_MAC  sound_light_wrover
 #define PERIPHERAL_CONTROLLER_MAC   peripheral
 #define REMOTE_CONTROLLER_MAC       remotecontrol
 #define BT_CONTROLLER_MAC       btremote
